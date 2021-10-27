5d940ce75cea6a052ca43040da0f8cb587580678
priscilla <pdjarbeng@outlook.com>
2021-10-27 11:52:50 -0400

    commit tree 4366fec54b3cb11187ee100849021c0debef9da0

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Code your solution in this file!
+
+let arrray = ['Sally', 'Bob', 'Freddy', 'Claudia']
+
+const returnFirstTwoDrivers = function(array){
+    return [ array[0], array[1]]
+}
+
+const returnLastTwoDrivers = function(array){
+    const length = array.length 
+    return [ array[length-2], array[length-1]]   //4-1 and 4-2
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(fare) {
+    return function(value) {
+        return fare * value
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+const selectDifferentDrivers = function (drivers, driversfunction) {
+    return driversfunction(drivers)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function receivesAFunction(callbackfunction) {
+//     callbackfunction()
+// }
+
+
+// function returnsANamedFunction() {
+//     return function greeting(){
+//         console.log("hello")
+//     }   
+// }
+
+
+// function returnsAnAnonymousFunction() {
+//     return function(){
+//     console.log('hi')
+//     }
+// }
+
